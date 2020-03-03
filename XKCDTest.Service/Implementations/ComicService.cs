41e6b3273ef254e622cda3bdc6efe9e62fb6b825
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2020-03-03 17:52:49 -0600

    Added Interface and implementation for Service Layer to get comic of the day

---
+++ XKCDTest.Service/Implementations/ComicService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XKCDTest.DTO.ViewModels;
+using XKCDTest.Service.Interfaces;
+using XKCDTest.Repository.Interfaces;
+
+namespace XKCDTest.Service.Implementations
+{
+    public class ComicService: IComicService
+    {
+        private readonly IComicRepo _comicRepo;
+
+        public ComicService(IComicRepo comicRepo)
+        {
+            _comicRepo = comicRepo;
+        }
+        public async Task<VMComicDetail> GetComicOfDay()
+        {
+            var comicOfDay = await _comicRepo.GetComicOfDay();
+            return comicOfDay;
+        }
+    }
+}
