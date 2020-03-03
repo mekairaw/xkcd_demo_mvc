70bdd8c518bdbb51e88cf37801d0a0d42f90ba12
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2020-03-03 16:28:56 -0600

    Added package for dependency injection, created new Interface for Comic Repository with its implementation to get the comic of the day

---
+++ XKCDTest.Repository/Interfaces/IComicRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XKCDTest.DTO.ViewModels;
+
+namespace XKCDTest.Repository.Interfaces
+{
+    public interface IComicRepo
+    {
+        Task<VMComicDetail> GetComicOfDay();
+    }
+}
