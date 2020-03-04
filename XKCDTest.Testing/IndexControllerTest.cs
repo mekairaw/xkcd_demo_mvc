ebd4435e91d5d4a2d69fb22a9c91330f478e7989
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2020-03-04 17:41:03 -0600

    Added unit testing to check if the View is rendering correctly a custom comic given an Id, fixed xUnit dependencies

--- XKCDTest.Testing/IndexControllerTest.cs
+++ XKCDTest.Testing/IndexControllerTest.cs
@@ -1,14 +1,59 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using Xunit;
+using XKCDTest.DTO.ViewModels;
+using XKCDTest.Service.Interfaces;
+using XKCDTest.Controllers;
 
 namespace XKCDTest.Testing
 {
     public class IndexControllerTest
     {
         [Fact]
-        public void Test1()
+        public async Task Test_Success_In_View_When_Getting_Comic_By_Id()
         {
+            //Arrange
+            int id = 500;
+            var mockService = new Mock<IComicService>();
+            mockService.Setup(s => s.GetCustomComic(id))
+                .ReturnsAsync(GetComicById(id));
+            var controller = new HomeController(mockService.Object);
+            //Act
+            var result = await controller.Index(id);
+            //Assert
+            var view = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<VMComic>(view.Model);
+            Assert.NotNull(model.Comic);
+            Assert.Equal(id, model.Comic.Num);
+            Assert.Equal(id, model.FirstComicId);
+            Assert.Equal(id, model.LastComicId);
+        }
 
+        private VMComic GetComicById(int id)
+        {
+            return new VMComic
+            {
+                Comic = new VMComicDetail
+                {
+                    Alt = "Testing",
+                    day = "04",
+                    Img = "http://xkcd.com",
+                    Link = "test.com",
+                    Month = "03",
+                    Num = id,
+                    SafeTitle = "Test Comic",
+                    Title = "Test Comic",
+                    Transcript = "Test",
+                    Yeat = "2020"
+                },
+                FirstComicId = id,
+                LastComicId = id,
+                NextComicId = null,
+                PreviousComicId = null
+            };
         }
     }
 }
