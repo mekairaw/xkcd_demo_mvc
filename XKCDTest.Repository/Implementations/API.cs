fa207ad0e0c1956efdd90fbe1cec00a7e7647016
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2020-03-04 20:11:47 -0600

    Removed 3rd party http client to use native cliente from .Net Core

---
+++ XKCDTest.Repository/Implementations/API.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+using XKCDTest.DTO.ViewModels;
+using XKCDTest.Repository.Interfaces;
+
+namespace XKCDTest.Repository.Implementations
+{
+    public class API: IAPI
+    {
+        private readonly HttpClient _client;
+
+        public API(HttpClient client)
+        {
+            _client = client;
+            _client.BaseAddress = new Uri("http://xkcd.com/");
+        }
+
+        public async Task<VMComicDetail> GetComicOfDay()
+        {
+            try
+            {
+                string endpoint = "/info.0.json";
+                var response = await _client.GetAsync(endpoint);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var result = JsonConvert.DeserializeObject<VMComicDetail>(await response.Content.ReadAsStringAsync());
+                    return result;
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<VMComicDetail> GetCustomComic(int? id)
+        {
+            try
+            {
+                string endpoint = string.Format("/{0}/info.0.json", id);
+                var response = await _client.GetAsync(endpoint);
+                if(response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var result = JsonConvert.DeserializeObject<VMComicDetail>(await response.Content.ReadAsStringAsync());
+                    return result;
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public Task<int?> GetFirstComicId()
+        {
+            return Task.Run(() => (int?)1);
+        }
+    }
+}
--- XKCDTest.Repository/Interfaces/IAPI.cs
+++ XKCDTest.Repository/Interfaces/IAPI.cs
@@ -2,16 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-using Refit;
 using XKCDTest.DTO.ViewModels;
 
 namespace XKCDTest.Repository.Interfaces
 {
     public interface IAPI
     {
-        [Get("/info.0.json")]
         Task<VMComicDetail> GetComicOfDay();
-        [Get("/{id}/info.0.json")]
+        //[Get("/{id}/info.0.json")]
         Task<VMComicDetail> GetCustomComic(int? id);
+        Task<int?> GetFirstComicId();
     }
 }
